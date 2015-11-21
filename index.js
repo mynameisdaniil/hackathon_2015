94ea2a0c1e555dc5b478cc8a2f406dbad704be9e
Daniil Sobol <sobol.daniil@gmail.com>
2015-11-22 01:35:27 +0300

    better support for timeouts

--- index.js
+++ index.js
@@ -115,10 +115,12 @@ app.post('/:token/play/control', function (req, res) {
   log(req.method, req.path, req.params.token);
   var key = 'session:' + req.params.token + ':control';
   var data = req.body;
-  if (data.timeout)
+  if (data.timeout) {
     setTimeout(function () {
       redis.set(key, encode({the_end: true}));
     }, data.timeout);
+    return res.sendStatus(200);
+  }
   redis.set(key, encode(req.body), function (e) {
     if (e) {
       err(e);
